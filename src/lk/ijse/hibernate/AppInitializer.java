237d1afd7b9f94099f0edd76bd0af95819365d5b
Ranger-HK <maxravindu99@gmail.com>
2024-02-12 17:11:55 +0530

    first commit

---
+++ src/lk/ijse/hibernate/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.hibernate;
+
+/**
+ * @Created By Ravindu Prathibha
+ * @created 2/12/2024 - 5:10 PM
+ * @project Hibernate
+ */
+public class AppInitializer {
+    public static void main(String[] args) {
+
+    }
+}
